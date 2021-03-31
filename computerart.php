ffb4322b3db989e8180c91722b82e5559720ca25
PULUTUR <167929@novacollege.nl>
2021-03-31 14:46:36 +0200

    colors!!

--- computerart.php
+++ computerart.php
@@ -1,5 +1,5 @@
-bwirw3e4h897w3hjtg8i9arhguioerhbntg
+<h1 style="color: red">bwirw3e4h897w3hjtg8i9arhguioerhbntg</h1>
 
-Hallo dit is een test om te kijken wat er gebeurt als ik alles verwijder en het niet push.
+<h2 style="color: blue">Hallo dit is een test om te kijken wat er gebeurt als ik alles verwijder en het niet push.</h2>
 
 Hier is niks meer te zien.. :'(
