d44f2f9cd170807f3b55ac744ac45d329bc4c346
Jimm2704 <169870@novacollege.nl>
2021-03-31 14:52:58 +0100

    Nog Een Kleine aanpassing123

--- computerart.php
+++ computerart.php
@@ -6,4 +6,4 @@ Hier is niks meer te zien.. :'(
 
 Hallo dit is een test om te kijken wat er gebeurt als ik alles verwijder en het niet push.
 
-en nu probeer ik nog wat erbij te voegen.
+en nu probeer ik nog wat erbij te voegen. 
