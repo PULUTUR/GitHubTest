518be4ad683ab2c8d76742fc66cb0292c4a19849
PULUTUR <167929@novacollege.nl>
2021-06-04 14:50:13 +0200

    Nieuw bericht

--- computerart.php
+++ computerart.php
@@ -6,4 +6,6 @@ Hier is niks meer te zien.. :'(
 
 Hallo dit is een test om te kijken wat er gebeurt als ik alles verwijder en het niet push.
 
-en nu probeer ik nog wat erbij te voegen. 
+en nu probeer ik nog wat erbij te voegen.
+
+Banaantjes zijn lekker
